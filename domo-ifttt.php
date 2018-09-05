284362a69405fcaf41456ce21bd757f7a4a5c8a6
The Duke <40343864+TheDukeSr@users.noreply.github.com>
2018-09-05 19:43:25 +0200

    Optional Favorites added.
    Groups and Secene support imrpoved.
    Bugfixes.

--- domo-ifttt.php
+++ domo-ifttt.php
@@ -1,5 +1,4 @@
 <?php
-
 // *** Change settings for your Domoticz Server here:
 // Protocol, IP/hostname and Port for Domoticz
 define('DOMO_SERVER', 'http://127.0.0.1:9090');
@@ -8,23 +7,26 @@ define('DOMO_SERVER', 'http://127.0.0.1:9090');
 // This password is used in the IFTTT Applets.
 define('PASSKEY', 'superSecretPasswordOnlyIFTTknows');
 
+// Use favorites only, or ALL devices (1 or 0).
+define('FAVONLY', '1');
+
 // *** DO NOT EDIT PASSED THIS LINE ***
 if($_REQUEST['passkey'] <> PASSKEY){
-	logLine('Invalid passkey');
-	exit;
+        logLine('Invalid passkey');
+        exit;
 };
 
 function logLine($line){
-	syslog(LOG_INFO, '[domoIfttt] '.$line);
-	echo '<li>'.$line.'</li>';
+        syslog(LOG_INFO, '[domoIfttt] '.$line);
+        echo '<li>'.$line.'</li>';
 };
 
 //Ping the domoticz API
 function domoApi($query){
-	$apiUrl = DOMO_SERVER.'/json.htm?'.$query;
-	$json = file_get_contents($apiUrl);
-	$array = json_decode($json, true);
-	return $array;
+        $apiUrl = DOMO_SERVER.'/json.htm?'.$query;
+        $json = file_get_contents($apiUrl);
+        $array = json_decode($json, true);
+        return $array;
 };
 
 //Toogle state for a device, group, or scene
@@ -32,17 +34,17 @@ function domoToggle($idx, $grouptype, $onOff = 'On'){
         if ($grouptype == 'Group')
                 $query = 'param=switchscene&type=command&idx='.$idx.'&switchcmd='.$onOff;
         elseif ($grouptype == 'Scene')
-		// Scenes can only be toggled ON
+                // Scenes can only be toggled ON
                 $query = 'param=switchscene&type=command&idx='.$idx.'&switchcmd=On';
         else
-		$query = 'param=switchlight&type=command&idx='.$idx.'&switchcmd='.$onOff;
-	return domoApi($query);
+                $query = 'param=switchlight&type=command&idx='.$idx.'&switchcmd='.$onOff;
+        return domoApi($query);
 };
 
-//Search through favorites devices from domoticz
+//Search through favorites LIGHT ONLY devices from domoticz
 //Returns as an multi array with only our device (Name, idx, Type)
 function getDevices($requestedDevice){
-        $query = 'type=devices&used=true&filter=all&favorite=1';
+        $query = 'type=devices&used=true&filter=light&favorite='.FAVONLY;
         $devArray = domoApi($query);
         $devicelisting = array();
         foreach($devArray['result'] as $d){
@@ -54,24 +56,38 @@ function getDevices($requestedDevice){
         return $devicelisting;
 };
 
+// Search all SCENES and GROUPS from Domoticz
+function getScenes($requestedDevice){
+        $query = 'type=scenes&used=true&filter=all&favorite='.FAVONLY;
+        $scnArray = domoApi($query);
+        $scenelisting = array();
+        foreach($scnArray['result'] as $d){
+                if ($requestedDevice == simplifyMatch($d['Name'])) {
+                        $scenelisting[] = array('name'=> $d['Name'],'idx' => $d['idx'], 'type' => $d['Type']);
+                        break;
+                }
+        };
+        return $scenelisting;
+};
+
 //Smish string down into just letters and numbers
 //This improves the changes of a proper match by ignoring spaces, punctuation, capitalisation, "the", "my" etc.
 function simplifyMatch($v){
-	$v = strtolower($v);
-	$v = removeUselessWord($v, 'the');
-	$v = removeUselessWord($v, 'my');
-	$v = removeUselessWord($v, 'our');
+        $v = strtolower($v);
+        $v = removeUselessWord($v, 'the');
+        $v = removeUselessWord($v, 'my');
+        $v = removeUselessWord($v, 'our');
 // Problem: Below 2 lines created spaces result in zero results.
 // Have to cleanup these 2 lines in the future.
 //      $v = preg_replace("/^[a-zA-Z0-9]+$/", '', $v);
 //      $v = str_replace(' ', '', $v);
-	return $v;
+        return $v;
 };
 
 function removeUselessWord($v, $w){
-	$len = strlen($w)+1;
-	if(substr($v,0,$len) == $w.' '){return substr($v,$len);};
-	return $v;
+        $len = strlen($w)+1;
+        if(substr($v,0,$len) == $w.' '){return substr($v,$len);};
+        return $v;
 };
 
 //Get device name
@@ -80,21 +96,26 @@ if($_REQUEST['devName']){
         logLine('Got request for device name: '.$_REQUEST['devName']);
         $requestedDevice = simplifyMatch($_REQUEST['devName']);
 }else{
-	logLine('No device name requested.');
-	exit;
+        logLine('No device name requested.');
+        exit;
 };
 
-//Check for a match in Domoticz
-// Changed to include support for multi array group and scenes
+//Check for a Light device match in Domoticz
 $deviceList = getDevices($requestedDevice);
 $idx = $deviceList[0]['idx'];
+// Seperate search for scenes and groups in Domoticz
+if(!$idx){
+        $deviceList = getScenes($requestedDevice);
+        $idx = $deviceList[0]['idx'];
+}
+// Parse result from Domotics Query
 if($idx){
         logLine('Matched on device Name: '.$deviceList[0]['name']);
         logLine('Matched on device IDX:  '.$deviceList[0]['idx']);
         logLine('Matched on device Type: '.$deviceList[0]['type']);
 }else{
-	logLine('Device not found');
-	exit;
+        logLine('Device not found');
+        exit;
 };
 
 //Execute the switching
@@ -108,8 +129,9 @@ if($requestedState){
 };
 
 if($toggleRes['status'] == 'OK'){
-	logLine('All OK - job done');
+        logLine('All OK - job done');
 }else{
-	logLine('Got error from Domoticz.  Status:'.$toggleRes['status']);
+        logLine('Got error from Domoticz.  Status:'.$toggleRes['status']);
 };
 ?>
+
